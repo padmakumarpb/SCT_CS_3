e60db5e2b575277bff928d36b05b0c37927ee13b
Padmakumar P B <padmakumarpb176@gmail.com>
2024-07-28 12:22:33 +0530

    Improved the code by adding checks for spaces, providing detailed feedback on missing criteria, and displaying password strength levels.

--- password_strength.py
+++ password_strength.py
@@ -1,9 +1,19 @@
 import re
 
 def main():
-    #Obtain the password from the user
-    password = input("Enter the password :")
-    check_password_strength(password)
+
+        print()
+
+        #Obtain the password from the user
+        password = input("Enter the password : ")
+
+        print()
+
+        if ' ' in password:
+            print("Password must not contain spaces.")
+        else:
+            check_password_strength(password)
+           
 
 def check_password_strength(password):
     length_criteria = len(password) >= 8
@@ -25,7 +35,22 @@ def check_password_strength(password):
         strength_of_password += 1
     
     strength_level = ["Very Weak", "Weak", "Moderate", "Strong", "Very Strong"]
+    print(f"Password strength: {strength_level[strength_of_password-1]}\n")
 
-    print(f"Password strength: {strength_level[strength_of_password-1]}")
+    if strength_of_password < 5:
+        print("Password must:")
+        if not length_criteria:
+            print(" - be more than 8 characters")
+        if not (upper_case_criteria and lower_case_criteria and number_criteria and special_character_criteria):
+            print(" - include the following:")
+            if not upper_case_criteria:
+                print("     - An uppercase letter")
+            if not lower_case_criteria:
+                print("     - A lowercase letter")
+            if not number_criteria:
+                print("     - A number")
+            if not special_character_criteria:
+                print('     - A special character (e.g., !@#$%^&*(),.?\":{}|<>)')
+        
 
 main()
